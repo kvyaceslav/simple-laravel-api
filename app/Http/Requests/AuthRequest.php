c6b4a4f00f28afea9351a14fed70f20b89b5c8cf
kvyaceslav <kvyaceslav@gmail.com>
2023-03-15 10:20:24 +0200

    Fix for validation

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Requests;
 
+use App\Constants\ValidationConstants;
+use App\Http\Controllers\API\BaseController;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AuthRequest extends FormRequest
 {
+    /**
+     * @var BaseController
+     */
+    private BaseController $baseController;
+
+    /**
+     * @param BaseController $baseController
+     */
+    public function __construct(BaseController $baseController)
+    {
+        $this->baseController = $baseController;
+    }
+
     /**
      * @return array
      */
@@ -15,5 +32,19 @@ class AuthRequest extends FormRequest
             'email' => 'required|email',
             'password' => 'required',
         ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return HttpResponseException
+     */
+    public function failedValidation(Validator $validator): HttpResponseException
+    {
+        throw new HttpResponseException(
+            $this->baseController->sendError(
+                ValidationConstants::ERROR,
+                $validator->errors()->messages()
+            )
+        );
     }
 }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Requests;
 
+use App\Constants\ValidationConstants;
+use App\Http\Controllers\API\BaseController;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class RegisterRequest extends FormRequest
 {
+    /**
+     * @var BaseController
+     */
+    private BaseController $baseController;
+
+    /**
+     * @param BaseController $baseController
+     */
+    public function __construct(BaseController $baseController)
+    {
+        $this->baseController = $baseController;
+    }
+
     /**
      * @return array
      */
@@ -17,5 +34,19 @@ class RegisterRequest extends FormRequest
             'password' => 'required|min:6',
             'confirm_password' => 'required|same:password'
         ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return HttpResponseException
+     */
+    public function failedValidation(Validator $validator): HttpResponseException
+    {
+        throw new HttpResponseException(
+            $this->baseController->sendError(
+                ValidationConstants::ERROR,
+                $validator->errors()->messages()
+            )
+        );
     }
 }
